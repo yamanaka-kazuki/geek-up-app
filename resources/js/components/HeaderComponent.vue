f4514dc7f717b60e908e8a540773500fa6895755
yamanaka-kazuki <kazuki.yamanaka0013@gmail.com>
2023-10-09 22:21:07 +0900

    first-commit

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-navbar type="is-info">
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img :src="'/images/geekup-logo.png'" alt="Geekup's Logo" />
+            </b-navbar-item>
+        </template>
+        <template #end>
+            <b-navbar-item>
+                <b-icon icon="menu" size="is-medium" type="is-white"> </b-icon>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
